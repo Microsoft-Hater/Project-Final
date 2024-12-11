e126510df4404beb24f3fc391259031875e605ff
BuildTools <unconfigured@null.spigotmc.org>
2024-12-11 22:52:14 +0000

    Very Final Commit

---
+++ Assets/Scripts/OpenUrl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Openurl(){
+		Application.OpenURL("https://github.com/Microsoft-Hater/Project-Final");
+	}
+}
